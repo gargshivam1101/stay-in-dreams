c6bb29d941885831a342a0afeb3df263b7f5c12a
Shivam Garg <shivam.garg@outlook.in>
2024-07-21 20:03:50 -0400

    Encrypt Password

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,16 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
   // some code
   console.log(req.body);
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
-  await newUser.save();
-  res.status(201).json("Success");
+  const encryptPwd = bcryptjs.hashSync(password, 10); // salt defaults to 10
+  const newUser = new User({ username, email, password: encryptPwd });
+  try {
+    await newUser.save();
+    res.status(201).json("Success");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
